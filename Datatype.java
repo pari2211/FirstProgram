b2b542d45308b1e7fb5b19eee917cff5655e13c6
pari2211 <114828063+pari2211@users.noreply.github.com>
2022-10-02 00:10:36 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,38 @@
+package firstday;
+
+// data identification
+public class Datatype {
+
+	public static void main(String[] args) {
+		String xyz="i am pramod";
+		
+		
+	   System.out.println("string priniting "+xyz);
+		
+		byte by= -128;           // range  -128 to 127
+		System.out.println("maximum storing of byte "+by);
+		
+		short sh = -2000;
+		System.out.println("value of short "+sh);    // range -32768 - 32767
+	 
+		int integer=2147483647;
+		System.out.println("value of int "+integer);   // -2147483648 to  2147483647.
+	
+	     long mobile= 75070827895L;
+         System.out.println("value of long "+mobile);
+         
+         double number= 12.22;
+         System.out.println("value of double "+number);
+         
+         float f=32.33F;
+         System.out.println("value of float "+f);
+         
+         char r='A';
+         System.out.println("value of character "+r);
+         
+         boolean flag =true;
+		 System.out.println("value of boolean  "+flag);
+		 
+	}
+
+}
---
+++ FIrstDay1.java
@@ -0,0 +1,12 @@
+package firstday;
+
+public class FIrstDay1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java");
+		System.out.print("Pramod");
+
+	}
+	
+}
